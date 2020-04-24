671b3f89990ac804460c5dbec3942aef198f9ab4
Morgan Matney <morgan.matney89@gmail.com>
2020-04-24 15:55:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,8 @@
-require 'pry'
-
 def turn_count(board)
   counter = 0
   board.each do |token|
     if token == "X" || token == "O"
       counter += 1
-      binding.pry
     end
   end
   counter
